1f9b43f6754ea016a4743bd0474767d23d6d56a6
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-05-27 14:15:30 -0700

    initial commmit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const app = express();
+app.use(express.static("public"));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}...`));
